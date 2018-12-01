3d54a6c0cf768ccfede5c84a320654472e0d46d9
Patton Nguyen <41391357+pnguyen1912@users.noreply.github.com>
2018-12-01 15:28:36 -0600

    Add files via upload

--- timer.js
+++ timer.js
@@ -5,14 +5,28 @@ class timer {
         let time = 59;
         let a = setInterval(function () {
             if (time > 0) {
-                if (count.style.color =='white'){
-                count.style.color = 'red';} else if (count.style.color =='red'){count.style.color ='green'} else {count.style.color = 'white'}
+                //get random color
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                console.log(color)
+                count.style.color = color;
+                // set color
+                // if (count.style.color =='white'){
+                // count.style.color = 'red';} 
+                // else if (count.style.color =='red')
+                // {count.style.color ='green'}
+                // else if (count.style.color =='green')
+                // {count.style.color ='blue'}
+                //  else {count.style.color = 'white'}
                 count.innerHTML = time;
                 time = time - 1;
             } else {
                 let sound1 = document.getElementById('sound');
                 sound1.play();
-                alert('time up')
+                alert('Happy New Year')
             }
         }, 1000);
         setTimeout(function () {
